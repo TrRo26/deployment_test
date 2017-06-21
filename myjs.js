917e2f3d52b9ceaee4ddfd6e417dff24399e7673
TrRo26 <travis_roy@yahoo.com>
2017-06-20 19:20:53 -0500

    Add appended text

--- myjs.js
+++ myjs.js
@@ -3,6 +3,7 @@ function addImage() {
 	$('#yep').on('click', function() {
 		var here = $("<img src='manGlitter.jpg'>")
 		$('.hook').append(here)
+		$('.hook').append("How does Deployment work???????????")
 	})
 }
 
